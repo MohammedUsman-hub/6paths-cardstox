96f9c1aaabd4975fe530c3af1bf70586f71ff3bf
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2022-06-22 12:19:25 +0100

    update 5

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import logo from "../images/logo.jpg"
 
 
 export default class NavBar extends Component {
     render() { 
         return (
-           console.log("TESTING!")
+           <nav>
+            <img src={logo} alt="" />
+            <button >Start</button>
+            <h3>6 Paths CardStox</h3>
+           </nav>
         );
     }
 }

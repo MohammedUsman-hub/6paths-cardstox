ca9b0f27f95a50c7df25246d28da1e8dd27d2dd7
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2022-06-28 18:31:44 +0100

    update 7

--- src/components/headerImg.js
+++ src/components/headerImg.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import jordanRookie from "../images/mj-rookie.jpg"
+import "../index.css"
 //import bg1 from "../images/stock-wallpaper-2.jpg"  <img src={bg1} alt="" ></img>
 
 
 export default class HeaderImg extends Component {
     render() { 
         return (
-            <div>
-                 <img src={jordanRookie} alt="" ></img>
-               
+            <div>   
+                 <img src={jordanRookie} alt="" ></img>   
             </div>
            
         );
     }
 }
- 
+
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,9 +8,11 @@ export default class NavBar extends Component {
            <nav>
             <img src={logo} alt="" />
             <button >Start Buying</button>
-            <h6>About Us</h6>
-            <h6>Your Collection</h6>
-            <h6>Market</h6>
+            <ul className='nav-links'>
+                <li>Market</li>
+                <li>Your Collection</li>
+                <li>About Us</li>
+            </ul>
            </nav>
         );
     }

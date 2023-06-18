c63e4289294d7950ecb1771b58b1b02e11d0ef23
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-06-18 19:11:49 +0100

    fixed background img 2

--- src/components/babe.js
+++ src/components/babe.js
@@ -8,8 +8,7 @@ export default class Ruth extends Component {
     render() { 
         return (
             <div className='ruth-div'>
-                <img id='ruth' src="./images/babe.jpg"></img>
-                <h2 id='ruth-h2'>A Market For Both Raw And Graded</h2>
+                
                 
             </div>
             
--- src/components/backgorund2.js
+++ src/components/backgorund2.js
@@ -12,13 +12,23 @@ const bg2Styling ={
     padding: "0px, 0px, 0px, 0px",
     margin: "0px, 0px, 0px, 0px",
 };
+const bg2Div={
+    position: 'relative',
+}
 
 export default class Background2 extends Component {
     
     render() { 
         return (
-            <div>
+            <div style={bg2Div}>
                 <img style={bg2Styling} src="./images/stock-wallpaper-1.jpg"></img>
+
+                <img  id='curry' src="./images/rpa-1.jpeg"></img>    
+                <h3 id='curry-h3'>Vitnage And Modern Cards.</h3>
+                <h3 id='curry-h3-1'> Most Common To The Most Rarest.</h3>
+
+                <img id='ruth' src="./images/babe.jpg"></img>
+                <h2 id='ruth-h2'>A Market For Both Raw And Graded</h2>
             </div>
 
 
